83b60fe1a7eb74ac2155c77d94dfad1d90141ace
Yujian Zhang <yujian.zhang@cepton.com>
2019-04-05 09:36:23 -0700

    change to sending length

--- led/led_serial_test.c
+++ led/led_serial_test.c
@@ -14,18 +14,25 @@ int main(void) {
     }
     char input_buff[256] = { 0 };
     int input_size = 0;
-    uint8_t num;
+    uint8_t num[16];
+    int i = 0;
     while (true) {
         fgets(input_buff, 256, stdin);
         input_size = strlen(input_buff);
-        num = atoi(input_buff);
-        if (input_size == 1) {
+        num[i] = atoi(input_buff);
+        if (input_size == 1 && i == 0) {
             printf("Quiting\n");
             break;
+        } else if (input_size == 1) {
+            serial_send(fd, num, i);
+            i = 0;
         } else {
-            serial_send(fd, &num, 1);
-            printf("Sending: %d\n", num);
+            i += 1;
         }
+        // else {
+        //     serial_send(fd, &num, 1);
+        //     printf("Sending: %d\n", num);
+        // }
     }
     serial_close(fd);
     return 0;
